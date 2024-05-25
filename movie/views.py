af4fbab87986ef6528ac0bb5439ea300069e207b
jenberu <jemberu0970@gmail.com>
2024-05-26 01:20:21 +0300

    second commit

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+def home(request):
+ return HttpResponse('<h1>Welcome to Home Page</h1>')
 # Create your views here.
+def about(request):
+ return HttpResponse('<h1>Welcome to About Page</h1>')
