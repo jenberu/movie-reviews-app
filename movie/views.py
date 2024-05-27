343718c2bc9db375960a848c6a54b30cd7f4dd65
jenberu <jemberu0970@gmail.com>
2024-05-27 16:03:00 +0300

    movie/views.py updated

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse
 from .models import Movie
+
 def home(request):
   searchTerm = request.GET.get('searchMovie')
   if searchTerm:
@@ -14,3 +15,10 @@ def about(request):
 def signup(request):
  email = request.GET.get('email')
  return render(request, 'signup.html', {'email':email})
+def detail(request,movie_id):
+  movie=get_object_or_404(Movie,pk=movie_id)
+  return render(request, 'detail.html', {'movie':movie})
+"""We use get_object_or_404 to get the specific movie object we want. 
+We provide movie_id as the primary key, pk=movie_id. If there is a match, 
+get_object_or_404, as its name suggests, returns us the object or the not 
+found (404) object """
